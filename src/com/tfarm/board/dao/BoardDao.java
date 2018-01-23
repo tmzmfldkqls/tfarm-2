a8343eeaf04feb82d933d6ce17c05721a3106c16
han <gksvlf25@naver.com>
2018-01-23 18:00:41 +0900

    dfdf

--- src/com/tfarm/board/dao/BoardDao.java
+++ src/com/tfarm/board/dao/BoardDao.java
@@ -1,5 +1,14 @@
 package com.tfarm.board.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import com.tfarm.board.model.BoardDto;
+
+
 public interface BoardDao {
+	
+	BoardDto viewArticle(int seq);
+	List<BoardDto> listArticle(Map<String, String> map);
 
 }
